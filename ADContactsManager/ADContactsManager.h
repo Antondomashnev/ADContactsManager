e0f8219b9fd4ccc5d1b2d424bbef112e2688e3ec
Anton Domashnev <antondomashnev@gmail.com>
2016-02-14 15:29:13 +0100

    Initial Commit

---
+++ ADContactsManager/ADContactsManager.h
@@ -0,0 +1,19 @@
+//
+//  ADContactsManager.h
+//  ADContactsManager
+//
+//  Created by Anton Domashnev on 2/14/16.
+//  Copyright © 2016 Anton Domashnev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ADContactsManager.
+FOUNDATION_EXPORT double ADContactsManagerVersionNumber;
+
+//! Project version string for ADContactsManager.
+FOUNDATION_EXPORT const unsigned char ADContactsManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ADContactsManager/PublicHeader.h>
+
+
